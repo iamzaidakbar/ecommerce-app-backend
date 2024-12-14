1d3e2ba7334019db12f1a7f6d00975d42dcd0fe9
iamzaidakbar <iamzaidakbar@gmail.com>
2024-12-14 23:46:45 +0530

    xx

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,15 @@
 import app from '../src/app';
+import { connectDB } from '../src/config/db';
+
+// Connect to MongoDB
+connectDB()
+  .then(() => console.log('MongoDB connected in serverless function'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Add request logging
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 export default app; 
